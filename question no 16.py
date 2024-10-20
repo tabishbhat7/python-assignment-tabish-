32b9d352e48856d20dae31c0e957ce49011ea119
tabishbhat7 <tabishbhat11415@gmail.com>
2024-10-20 23:31:42 +0530

    Add files via upload

---
+++ question no 16.py
@@ -0,0 +1,12 @@
+a=(input("Enter the first side (a)"))
+b=(input("Enter the second side (b)"))
+c=(input("Enter the third side (c)"))
+if a + b > c and a + c > b and b + c > a:
+    if a == b == c:
+        print("The triangle is equilateral.")
+    elif a == b or b == c or a == c:
+        print("The triangle is isosceles.")
+    elif a == b and b == c and a == c:
+        print("The triangle is scalene.")
+else:
+    print("The triangle is not valid.")
