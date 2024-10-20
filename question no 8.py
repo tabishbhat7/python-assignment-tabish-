ee5d9caed504d01eb24accbbddd62f964cfe84e6
tabishbhat7 <tabishbhat11415@gmail.com>
2024-10-20 22:46:09 +0530

    Add files via upload

---
+++ question no 8.py
@@ -0,0 +1,6 @@
+character=input("Enter an alphabet: ")
+if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
+    print(character, "is a vowel")
+else:
+    print(character, "is a consonant")
+
