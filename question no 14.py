b0526f0d6612454136cb716228a9a96514a4726b
tabishbhat7 <tabishbhat11415@gmail.com>
2024-10-20 23:23:35 +0530

    Add files via upload

---
+++ question no 14.py
@@ -0,0 +1,8 @@
+
+x =(input("Enter the first angle"))
+y =(input("Enter the second angle"))
+z =(input("Enter the third angle"))
+if x + y + z == 180:
+    print("The triangle is valid")
+else:
+    print("The triangle is not valid.")
